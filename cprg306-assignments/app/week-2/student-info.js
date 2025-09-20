1b095bd0112da1059b4fb93d55a8bf1dc9429faf
Abdullah <abdullahdauda55@gmail.com>
2025-09-19 20:50:19 -0600

    Complete Week 2 Assignment

---
+++ cprg306-assignments/app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    
+    return (
+    <main>
+        <h3>Abdullah Dauda</h3>
+        <Link href="https://github.com/AbdullahDauda/cprg306-assignments">GitHub Repository</Link>
+    </main>
+    );
+}
