f03e0fe31a7924b27e325c5a1ab4af9452489845
Abdullah <abdullahdauda55@gmail.com>
2025-11-27 14:58:42 -0700

    add week-9

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -11,6 +11,7 @@ export default function Page() {
         <Link href="./week-6">Week 6 Assignment <br/></Link>
         <Link href="./week-7">Week 7 Assignment <br/></Link>
         <Link href="./week-8">Week 8 Assignment <br/></Link>
+        <Link href="./week-9">Week 9 Assignment <br/></Link>
 
     </main>
     );
